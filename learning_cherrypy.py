e5d76cf249d2c3ec58d846ede6098d6e2781b735
Anurag <anurag.rawat@thesocialbytes.com>
2020-05-02 08:21:05 +0530

    changes in the learning_cherrypy.py

--- learning_cherrypy.py
+++ learning_cherrypy.py
@@ -1,5 +1,7 @@
 import cherrypy
 from config import  LOG_CONF
+import glob
+import os.path
 import logging
 import logging.config
 logger = logging.getLogger()
@@ -9,7 +11,6 @@ db_logger = logging.getLogger('db')
 
 class Blog(object):
     @cherrypy.expose
-    dd = "jhj"
     def index(self):
         return "Hello THis my First Blog................"
 
@@ -24,14 +25,58 @@ class Forum(object):
 
 class Root(object):
     @cherrypy.expose
-    def index(self):
-        logger.info('boom')
-        db_logger.info('bam')
-        cherrypy.log("hello there") # for logging the errors
-        cherrypy.response.cookie["seckret_key"]="asdasdasdasda"
-        google_appid = {"naam":"Anurag"}
-        return cherrypy.request.cookie["seckret_key"]
+    def index(self, directory="."):
+        html = """<html><body><h2>Here are the files in the selected directory:</h2>
+        <a href="index?directory=%s">Up</a><br />
+        """ % os.path.dirname(os.path.abspath(directory))
 
+        for filename in glob.glob(directory + '/*'):
+            absPath = os.path.abspath(filename)
+            if os.path.isdir(absPath):
+                html += '<a href="/index?directory=' + absPath + '">' + os.path.basename(filename) + "</a> <br />"
+            else:
+                html += '<a href="/download/?filepath=' + absPath + '">' + os.path.basename(filename) + "</a> <br />"
+
+        html += """</body></html>"""
+        return html
+
+class MyCookieApp(object):
+    @cherrypy.expose
+    def set(self):
+        cookie = cherrypy.response.cookie
+        print (cookie)
+        cookie['userid'] = '12'
+        cookie["path"] = "127.0.0.2/cookies"
+        
+        return "<html><body>Hello, I just sent you a cookie</body></html>"
+    @cherrypy.expose
+    def index(self):
+        cookie = cherrypy.request.cookie
+        res = """<html><body>Hi, you sent me %s cookies.<br />
+                Here is a list of cookie names/values:<br />""" % len(cookie)
+        for name in cookie.keys():
+            res += "name: %s, value: %s<br>" % (name, cookie[name].value)
+        return res + "</body></html>"
+
+
+#allow Files downlaoding
+from cherrypy.lib.static import serve_file
+class Downloading(object):
+    @cherrypy.expose
+    def index(self,filepath):
+        return serve_file(filepath,"application/x-download",'attachment')
+    
+
+#for encoding the json data
+class Jsondata(object):
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def index(self):
+        return {"key":"Value"}
+
+
+
+'''Configuration for the serever'''        
 cherrypy.config.update({'server.socket_port': 9090})
 cherrypy.config.update({'log.screen':False,
                         'log.access_file':'',
@@ -40,9 +85,15 @@ cherrypy.engine.unsubscribe('graceful',cherrypy.log.reopen_files)
 logging.config.dictConfig(LOG_CONF)
 
 ''' cherry/.tree.mount  is for muliple application'''
+#routes defined
+cherrypy.tree.mount(Downloading(), '/download')
 cherrypy.tree.mount(Forum(), '/forum')
 cherrypy.tree.mount(Blog(), '/blog')
-cherrypy.tree.mount(Root(),'/root')
+root = Root()
+root.download = Downloading()
+cherrypy.tree.mount(root,'/root')
+cherrypy.tree.mount(MyCookieApp(),'/cookies')
+cherrypy.tree.mount(Jsondata(),"/jsondata")
 
 cherrypy.engine.start()
 cherrypy.engine.block()
